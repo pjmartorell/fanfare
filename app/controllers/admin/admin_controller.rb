b8997df97de2c705237e8bd36e063f4d35717602
Pere Joan Martorell <perejoan@itnig.net>
2014-06-19 06:30:36 +0200

    Adding all abilities to admin role. Require admin role to enter in backoffice

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,5 +1,17 @@
 class Admin::AdminController < ApplicationController
+  before_filter :require_admin
 
   layout "backoffice"
 
+  private
+
+  def require_admin
+    if !current_user
+      flash[:error] = "Necessites estar autenticat."
+      redirect_to new_user_session_path
+    elsif !current_user.admin?
+      flash[:error] = "Necessites ser administrador."
+      redirect_to root_path
+    end
+  end
 end
