86aaedc5b8738dd5f6fd396a5462d3359b34d496
Pere Joan Martorell <perejoan@itnig.net>
2014-06-19 06:40:57 +0200

    Changing admin boolean in Users for role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ActiveRecord::Base
+  ROLES = %w{ user admin }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :orders
+
+  ROLES.each do |app_role|
+    define_method("is_#{app_role}?") do
+      role == app_role
+    end
+  end
 end
