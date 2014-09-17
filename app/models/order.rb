c25f0aa0e8d4f4feec55fadbc02c62c8f8b3b284
Pere Joan Martorell <perejoan@itnig.net>
2014-09-17 16:04:32 +0200

    Setting reference and calculating bonus points for orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,32 @@
 class Order < ActiveRecord::Base
   SHIPPING_COUNTRIES = ['Spain']
 
-  belongs_to :user
   has_many :order_products
   has_many :products, :through => :order_products
+  belongs_to :user
 
   accepts_nested_attributes_for :order_products
+
+  before_validation :set_ref, :on => :create
+  before_validation :set_price, :on => :create
+  before_validation :set_bonus_points, :on => :create
+
+  private
+
+  def set_price
+    self.price = calculate_price.round(2)
+  end
+
+  def set_bonus_points
+    self.bonus_points = calculate_bonus_points
+  end
+
+  def set_ref
+    return unless user.present?
+    self.ref = calculate_ref
+  end
+
+  def calculate_ref
+    [Time.now.strftime("%Y%m"), user.id, user.orders.count].join
+  end
 end
