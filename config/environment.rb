87cb2e33d402ef0b00d18798c98e8fef45d5fd32
Pere Joan Martorell <perejoan@itnig.net>
2015-05-17 22:01:46 +0200

    Fixing heroku config

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,5 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-# Load APP configuration
-require "#{Rails.root}/config/setup_app"
-
 # Initialize the Rails application.
 Fanfare::Application.initialize!
